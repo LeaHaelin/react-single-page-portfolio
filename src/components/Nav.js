753a9267f549dbe71226994dd3c275cc889fe939
LeaHaelin <lea8haelin@gmail.com>
2023-03-01 00:40:02 +0100

    refactor styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,10 +6,10 @@ export const Nav = ({ iconGithub, iconFrontendM, iconLinkedin, iconTwitter }) =>
         <div className='nav banner'>
             <h3 className='banner__logo'>adamkeyes</h3>
             <div className='banner__icons'>
-                <img src={iconGithub} alt="github icon" className="banner__icon" />
-                <img src={iconFrontendM} alt="frontend mentor icon" className="banner__icon" />
-                <img src={iconLinkedin} alt="linkedin icon" className="banner__icon" />
-                <img src={iconTwitter} alt="twitter icon" className="banner__icon" />
+                <a href="#"><img src={iconGithub} alt="github icon" className="banner__icon" /></a>
+                <a href="#"><img src={iconFrontendM} alt="frontend mentor icon" className="banner__icon" /></a>
+                <a href="#"><img src={iconLinkedin} alt="linkedin icon" className="banner__icon" /></a>
+                <a href="#"><img src={iconTwitter} alt="twitter icon" className="banner__icon" /></a>
             </div>
         </div>
     )
