65c7572dad0a612754e9b07e602ed9e5f24ef347
LeaHaelin <lea8haelin@gmail.com>
2023-03-17 12:13:31 +0100

    refactor app

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import "./styles/app.css";
-import iconGithub from "./assets/images/icon-github.svg";
-import iconFrontendM from "./assets/images/icon-frontend-mentor.svg";
-import iconLinkedin from "./assets/images/icon-linkedin.svg";
-import iconTwitter from "./assets/images/icon-twitter.svg";
+
 
 function App() {
 
   return (
     <div className="App">
-      <Nav iconGithub={iconGithub} iconFrontendM={iconFrontendM} iconLinkedin={iconLinkedin} iconTwitter={iconTwitter} />
+      <Nav />
       <Hero />
       <hr className="line top-line" />
       <Skills />
@@ -23,7 +20,7 @@ function App() {
       <Projects />
       <Contact />
       <hr className="line bottom-line" />
-      <Footer iconGithub={iconGithub} iconFrontendM={iconFrontendM} iconLinkedin={iconLinkedin} iconTwitter={iconTwitter} />
+      <Footer />
     </div>
   );
 }
